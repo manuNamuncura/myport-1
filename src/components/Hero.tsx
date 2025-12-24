69371fe392f80de1ef3450bc439233d1679b087d
manuNamuncura <manu.namun94@gmail.com>
2025-12-24 02:07:09 -0300

    feat: proban url para ver cv

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -99,7 +99,7 @@ const Hero = () => {
                 className="w-40 bg-linear-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 text-white shadow-lg hover:shadow-xl transition-all duration-300"
               >
                 <a
-                  href="/public/cv.html"
+                  href="https://myport-1.vercel.app/cv.html"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex items-center"
